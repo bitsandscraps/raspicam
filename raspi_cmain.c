196572c433445c28997617d5c0c65f9fcb5e5525
bitsandscraps <qkrwhddml@kaist.ac.kr>
2016-09-01 01:45:56 +0900

    resize

--- raspi_cmain.c
+++ raspi_cmain.c
@@ -286,8 +286,11 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char* argv[]) {
     processed_image = ProcessImage(raw_image);
     FreeImage(&rpic, index);
     // Create windows and show the images.
-    cvNamedWindow("Raw", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("Processed", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("Raw", CV_WINDOW_NORMAL);
+    cvNamedWindow("Processed", CV_WINDOW_NORMAL);
+    cvResizeWindow("Raw", 480, 360);
+    cvResizeWindow("Processed", 480, 360);
+    cvMoveWindow("Processed", 0, 420);
     cvShowImage("Raw", raw_image);
     cvShowImage("Processed", processed_image);
     cvReleaseImage(&processed_image);
